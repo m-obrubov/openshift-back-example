95ce31041adc7b26def045f3fa01c0904a202e77
Maxim Obrubov <maxim.obrubov@bsc-ideas.com>
2020-11-22 19:49:02 +0300

    Initial

---
+++ src/main/java/ru/obrubov/os/backexample/BackExampleApplication.java
@@ -0,0 +1,13 @@
+package ru.obrubov.os.backexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackExampleApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/obrubov/os/backexample/HelloWorldController.java
@@ -0,0 +1,18 @@
+package ru.obrubov.os.backexample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mobrubov
+ * Created on 22.11.2020
+ */
+@RequestMapping("/hello")
+@RestController
+public class HelloWorldController {
+    @GetMapping
+    public String hello() {
+        return "Fuck you";
+    }
+}
